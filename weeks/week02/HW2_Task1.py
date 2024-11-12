911d13d3b039b75babcc4beb8be3eb637dfada1f
Kate-Surta <surta.ekaterina@gmail.com>
2024-11-12 16:25:08 +0100

    Update HW2_Task1.py

--- weeks/week02/HW2_Task1.py
+++ weeks/week02/HW2_Task1.py
@@ -1,16 +1,14 @@
 my_list = [0,-12,8,-44,461]
-my_list_lenth = len(my_list)
-for i in range(my_list_lenth):
-    for j in range(my_list_lenth - i - 1):
+for i in range(len(my_list)):
+    for j in range(len(my_list) - i - 1):
         if my_list[j] > my_list[j+1]:
            my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
            print(my_list)
 
 #bubble_sort_desc
 my_list = [0,-12,8,-44,461]
-my_list_lenth = len(my_list)
-for i in range(my_list_lenth):
-    for j in range(my_list_lenth - i - 1):
+for i in range(len(my_list)):
+    for j in range(len(my_list) - i - 1):
         if my_list[j] < my_list[j+1]:
            my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
            print(my_list)
@@ -18,13 +16,12 @@ for i in range(my_list_lenth):
 
 # bubble_sort_withEarlyStop
 my_list = [0, -12, 8, -44, 461, 0, 666, 999]
-my_list_lenth = len(my_list)
-for i in range(my_list_lenth):
+for i in range(len(my_list)):
     isSwapped = False
-    for j in range(my_list_lenth - i - 1):
+    for j in range(len(my_list) - i - 1):
         if my_list[j] > my_list[j + 1]:
             my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
             isSwapped = True
-            #print(my_list)
+            print(my_list)
     if not isSwapped:
         break
